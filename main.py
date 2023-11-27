9bc41b4e0d028d15484bf3b6446ed7387cd4522e
king liam <52921954+king0billy@users.noreply.github.com>
2023-11-27 21:22:44 +0800

    Chatgpt Update main.py
    chatgpt update

--- main.py
+++ main.py
@@ -8,36 +8,53 @@ TG_BOT_TOKEN = os.environ.get('TGBOT')
 TG_USER_ID = os.environ.get('TGUSERID')
 
 
-def checkin(email=os.environ.get('EMAIL'), password=os.environ.get('PASSWORD'),
-            base_url=os.environ.get('BASE_URL'), ):
+def checkin(email=os.environ.get('EMAIL'), password=os.environ.get('PASSWORD'), base_url=os.environ.get('BASE_URL')):
     email = email.split('@')
     email = email[0] + '%40' + email[1]
     session = requests.session()
-    session.get(base_url, verify=False)
+
+    try:
+        response = session.get(base_url)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Error connecting to {base_url}: {e}")
+        return
+
     login_url = base_url + '/auth/login'
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/56.0.2924.87 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
     }
-    post_data = 'email=' + email + '&passwd=' + password + '&code='
-    post_data = post_data.encode()
-    response = session.post(login_url, post_data, headers=headers, verify=False)
+
+    post_data = {'email': email, 'passwd': password, 'code': ''}
+    
+    try:
+        response = session.post(login_url, data=post_data, headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Error during login: {e}")
+        return
+
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/56.0.2924.87 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
         'Referer': base_url + '/user'
     }
-    response = session.post(base_url + '/user/checkin', headers=headers,
-                            verify=False)
-    response = json.loads(response.text)
-    print(response['msg'])
-    return response['msg']
+    
+    try:
+        response = session.post(base_url + '/user/checkin', headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Error during check-in: {e}")
+        return
+
+    response_data = response.json()
+    print(response_data['msg'])
+    return response_data['msg']
 
 
 result = checkin()
+
+# Rest of your script for notifications
 if SCKEY != '':
     sendurl = 'https://sctapi.ftqq.com/' + SCKEY + '.send?title=机场签到&desp=' + result
     r = requests.get(url=sendurl)
